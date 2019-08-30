383beac0388201867bffb8e31d58ad43f0abc563
DeNDevils <53941361+DeNDevils@users.noreply.github.com>
2019-08-30 09:45:43 +0700

    DeNDevils.py

---
+++ DeNDevils.py
@@ -0,0 +1,273 @@
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+echo
+echo
+python2 AIT.py
+echo
+clear
+echo
+echo $red"          Tool akan di install dalam waktu 5 detik"
+echo $red"            harap sabar menunggu yaa tod :v("   
+sleep 1
+echo '''\a
+\033[34;1m                              _
+\033[34;1m                             / |
+\033[34;1m                             | |
+\033[34;1m                             | |
+\033[34;1m                            _|_|_ '''
+sleep 1
+apt update && apt upgrade
+apt install nano
+apt install git
+pkg install python python2 vim figlet curl
+clear
+echo
+echo
+echo
+echo
+echo $ku"         Tool akan di install dalam waktu 5 detik"
+echo $ku"            harap sabar menunggu yaa tod :v"
+sleep 1
+echo '''\a
+\033[32;1m                             ____
+\033[32;1m                            |___ \
+\033[32;1m                             __) |
+\033[32;1m                            / __/
+\033[32;1m                           |_____| '''
+sleep 1
+apt install php
+pip2 install mechanize
+pip2 install lolcat
+pip2 install requests
+clear
+echo
+echo
+echo
+echo
+echo $i"         Tool akan di install dalam waktu 5 detik"
+echo $i"            harap sabar menunggu yaa tod :v"
+sleep 1
+echo '''\a
+\033[35;1m                            _____
+\033[35;1m                           |___ /
+\033[35;1m                             |_ \
+\033[35;1m                            ___) |
+\033[35;1m                           |____/ '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $bi"         Tool akan di install dalam waktu 5 detik"
+echo $bi"            harap sabar menunggu yaa sayang :v"
+sleep 1
+echo '''\a
+\033[33;1m                            _  _
+\033[33;1m                           | || |
+\033[33;1m                           | || |_
+\033[33;1m                           |__   _|
+\033[33;1m                              |_| '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $pur"         Tool akan di install dalam waktu 5 detik"
+echo $pur"            harap sabar menunggu yaa sayang :v"
+sleep 1
+echo '''\a
+\033[36;1m                             ____
+\033[36;1m                            | ___|
+\033[36;1m                            |___ \
+\033[36;1m                             ___) |
+\033[36;1m                            |____/ '''
+sleep 1
+clear
+echo
+echo $pur"======================================================"
+echo $i  "###"$pu"  AUTHOR  :  DeNDevils  "$bi"              ###"
+echo $i  "###"$pu"  WhatsApp:  083874961264"$bi"             ###"
+echo $i  "###"$pu"  GMAIL   :  vandeta404@gmail.com"$bi"     ###"
+echo $i  "###"$pu"  TEAM    :  ACTION INFINITY TEAM"$bi"     ###"
+echo $pur"======================================================"
+echo
+echo $i"MENU PILIHAN:"
+echo $cy"["$me"1"$cy"]"$bi" REPORT FB"
+echo $cy"["$me"2"$cy"]"$bi" BRUTE PORCE"
+echo $cy"["$me"3"$cy"]"$bi" BRUTE FORCE FB CRACKER"
+echo $cy"["$me"4"$cy"]"$bi" MBF"
+echo $cy"["$me"5"$cy"]"$bi" CLONING YAHOO"
+echo $cy"["$me"6"$cy"]"$bi" OSIF"
+echo $cy"["$me"7"$cy"]"$bi" PROFILEGUARD FB"
+echo $cy"["$me"8"$cy"]"$bi" BOT REACTION FACEBOOK"
+echo $cy"["$me"9"$cy"]"$bi" SPAM CALL"
+echo $cy"["$me"10"$cy"]"$bi" SPAM WHATSHAPP"
+echo $cy"["$me"11"$cy"]"$bi" YOUTUBE-DL"
+echo $cy"["$me"12"$cy"]"$bi" LACAK LOKASI"
+echo $cy"["$me"13"$cy"]"$bi" SPAM SMS BRUTAL TERBARU"
+echo $pur"["$i"14"$pur"]"$ku" EXIT"
+echo
+echo $bi"DeNDevils["$bi"Masukkan pilihan anda"$bi"]"
+read -p"DeNDevils>> " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/IlayTamvan/Report.git
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Senitopeng/fbbrute.git
+cd fbbrute
+python2 jomblo.py
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/Fb-Cracker-v.3.git
+cd Fb-Cracker-v.3
+python2 crack.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/tikuskecil/multi-bruteforce-facebook.git
+cd multi-bruteforce-facebook
+python2 MBF.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/wahyuandhika/YahooCloning.git
+cd YahooCloning
+pip2 install requests mechanize
+pip2 install requirements
+python2 cloning.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/CiKu370/OSIF.git
+cd OSIF
+pip2 install -r requirements.txt
+python2 osif.py
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/jaxBCD/FBshield.git
+cd FBshield
+python2 guard.py
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/edi-nasa12/ROBOT.git
+cd ROBOT
+python2 blackbot.py
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Aditya021/SpamCall
+cd SpamCall
+php SpamCall.php
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/4L13199/LITESPAM.git
+cd LITESPAM
+sh LITESPAM.sh
+fi
+
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/rahmadxyz/sub-bot.git
+cd sub-bot
+php sub-bot.php
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/locator.git
+cd locator
+bash locator.sh
+fi
+
+if [ $pil = 13 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/TERMUXID3/brutal-sms
+cd brutal-sms
+python2 run.py
+fi
+
+
+if [ $pil = 14 ]
+then
+clear
+figlet -f slant "W A I T"|Ulolcat
+sleep 2
+echo $cy"Terima Kasih sudah mengunakan Tools ini"
+sleep 2
+echo $i"Silahkan di pakai semoga bermanfaat Buat anda"
+sleep 2
+echo $pur"Bila Ada Kesalahan Anda Bisa Nanya Melalui Via"
+sleep 2
+echo $ku"Facebook :"$i" DeNDevils"
+echo $ku"WhatsApp :"$i" 083874961264"
+sleep 2
+echo $pur"Terima kasih Yang Sudah Support dengan Tools ini"
+sleep 2
+exit
+fi
